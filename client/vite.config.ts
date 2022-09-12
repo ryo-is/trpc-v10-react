3d6927105edc234206a0ef41ffc66d189b001f07
ryo-is <ryosuke.izumi62@gmail.com>
2022-09-13 08:19:55 +0900

    feat: add client

---
+++ client/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => {
+  return {
+    server: {
+      port: 9999,
+    },
+    plugins: [react()],
+  };
+});
